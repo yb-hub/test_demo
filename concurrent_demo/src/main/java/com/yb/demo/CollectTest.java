e5a9ae3a5148c432438ddd5f50a62f4013dc1ad2
yang <840823561@qq.com>
2021-11-24 17:01:46 +0800

    add mongodb_demo

--- concurrent_demo/src/main/java/com/yb/demo/CollectTest.java
+++ concurrent_demo/src/main/java/com/yb/demo/CollectTest.java
@@ -1,8 +1,10 @@
 package com.yb.demo;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * @author yb
@@ -15,6 +17,48 @@ public class CollectTest {
     public static void main(String[] args) throws InterruptedException {
         iteratorTest();
         //arrayListTest();
+        //vectorTest();
+        //arrayListTest2();
+    }
+
+    private static void arrayListTest2() {
+        ArrayList<Integer> ls = new ArrayList<>();
+        List<Integer> list = Collections.synchronizedList(ls);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()){
+            Integer num = iterator.next();
+            if(num == 2){
+                list.remove(num);
+            }
+            System.out.println(num);
+        }
+        listTest(list);
+
+    }
+
+    private static void vectorTest() {
+        Vector<Integer> list = new Vector<>();
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+        Iterator<Integer> iterator = list.iterator();
+//        while (iterator.hasNext()){
+//            Integer num = iterator.next();
+//            if(num == 2){
+//                list.remove(num);
+//            }
+//            System.out.println(num);
+//        }
+
+        listTest(list);
+
     }
 
     /**
@@ -35,20 +79,33 @@ public class CollectTest {
             }
             System.out.println(num);
         }
+        listTest(list);
     }
 
     private static void arrayListTest() throws InterruptedException {
-        AtomicInteger number =  new AtomicInteger();
-        ArrayList<Integer> list = new ArrayList<>();
+        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
         Iterator<Integer> iterator = list.iterator();
-        //CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>();
-        for (int i = 0; i < 300; i++) {
+        while (iterator.hasNext()){
+            Integer num = iterator.next();
+            if(num == 2){
+                list.remove(num);
+            }
+            System.out.println(num);
+        }
+        listTest(list);
+    }
+
+    private static void listTest(List list){
+        for (int i = 0; i < 30; i++) {
             new Thread(()->{
-                list.add(number.getAndIncrement());
-                //System.out.println(list.get(0));
+                list.add(UUID.randomUUID());
+                System.out.println(list);
             }).start();
         }
-        Thread.sleep(1000);
-        System.out.println(list);
     }
 }
